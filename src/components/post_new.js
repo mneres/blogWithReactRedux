833fb5024022701778a275902842bcd7353aa460
Marcelo Neres <marcelo.neres1@gmail.com>
2018-08-01 23:44:04 +0100

    Added create post action

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
   renderField(field){
+    const { meta: {touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text" 
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
   onSubmit(values){
-    console.log(values);
+    this.props.history.push('/');
+    this.props.createPost(values);
   }
 
   render(){
@@ -42,6 +50,9 @@ class PostsNew extends Component {
             component={this.renderField}
           />
           <button type="submit" className="btn btn-primary">Submit</button>
+          <Link className="btn btn-danger" to="/">
+            Cancel
+          </Link>
         </form>
       </div>
     );
@@ -65,4 +76,6 @@ function validate(values){
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-}) (PostsNew);
+}) (
+  connect(null, { createPost })(PostsNew)
+);
